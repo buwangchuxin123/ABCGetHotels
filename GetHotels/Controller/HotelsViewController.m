23280e6ee653b9f4754f1a885f5d6d4f216939b2
wangmin <dearwang123@gmail.com>
2017-08-24 20:52:32 +0800

    首页

--- GetHotels/Controller/HotelsViewController.m
+++ GetHotels/Controller/HotelsViewController.m
@@ -8,14 +8,18 @@
 
 #import "HotelsViewController.h"
 
-@interface HotelsViewController ()
-
+@interface HotelsViewController (){
+    
+    }
 @end
 
 @implementation HotelsViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+  //  [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+   // [self naviConfig];
+ 
     // Do any additional setup after loading the view.
 }
 
@@ -23,6 +27,29 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)naviConfig{
+   // self.navigationItem.title = @"GetHotels";
+    //设置导航条的颜色（风格颜色）
+   // self.navigationController.navigationBar.barTintColor = [UIColor blueColor];
+    //设置导航条标题颜色
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+    //设置导航条是否被隐藏
+    self.navigationController.navigationBar.hidden = NO;
+    
+    //设置导航条上按钮的风格颜色
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    //设置是否需要毛玻璃效果
+    self.navigationController.navigationBar.translucent = YES;
+    //为导航条左上角创建一个按钮
+//    UIBarButtonItem *left = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop target:self action:@selector(backAction)];
+//    self.navigationItem.leftBarButtonItem = left;
+}
+
+- (void)backAction{
+
+}
+
 
 /*
 #pragma mark - Navigation
