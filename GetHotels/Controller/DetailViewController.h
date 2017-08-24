972c5c2a787bfcc46fac5bb759126811d9a94700
wangmin <dearwang123@gmail.com>
2017-08-24 10:27:24 +0800

    新建了类

---
+++ GetHotels/Controller/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  GetHotels
+//
+//  Created by admin on 2017/8/24.
+//  Copyright © 2017年 EDucation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ GetHotels/Controller/PayViewController.h
@@ -0,0 +1,13 @@
+//
+//  PayViewController.h
+//  GetHotels
+//
+//  Created by admin on 2017/8/24.
+//  Copyright © 2017年 EDucation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PayViewController : UIViewController
+
+@end
