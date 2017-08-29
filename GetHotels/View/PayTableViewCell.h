97962f5e0e195abd361cbbeec61240ca3f8cffcf
YiqiangQian <2313530400@qq.com>
2017-08-29 20:15:13 +0800

    新增cell类

---
+++ GetHotels/View/PayTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PayTableViewCell.h
+//  GetHotels
+//
+//  Created by admin on 2017/8/29.
+//  Copyright © 2017年 EDucation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PayTableViewCell : UITableViewCell
+
+@end
