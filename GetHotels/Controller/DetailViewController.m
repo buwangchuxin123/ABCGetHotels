a63c61bb2d2edc9bf0290e193b0dce8f1de34ba6
wangmin <dearwang123@gmail.com>
2017-08-29 18:02:25 +0800

    detail拉好了控件

--- GetHotels/Controller/DetailViewController.m
+++ GetHotels/Controller/DetailViewController.m
@@ -9,6 +9,35 @@
 #import "DetailViewController.h"
 
 @interface DetailViewController ()
+@property (weak, nonatomic) IBOutlet UIView *adView;
+@property (weak, nonatomic) IBOutlet UILabel *hotelName;
+@property (weak, nonatomic) IBOutlet UILabel *price;
+@property (weak, nonatomic) IBOutlet UILabel *address;
+@property (weak, nonatomic) IBOutlet UIButton *starDayBtn;
+@property (weak, nonatomic) IBOutlet UILabel *starDayLbl;
+@property (weak, nonatomic) IBOutlet UIButton *endDayBtn;
+@property (weak, nonatomic) IBOutlet UILabel *endDayLbl;
+@property (weak, nonatomic) IBOutlet UIImageView *UIimage;
+@property (weak, nonatomic) IBOutlet UIButton *roomTypeBtn;
+@property (weak, nonatomic) IBOutlet UILabel *breakfastLbl;
+@property (weak, nonatomic) IBOutlet UILabel *bedTypeLbl;
+@property (weak, nonatomic) IBOutlet UILabel *roomArea;
+@property (weak, nonatomic) IBOutlet UILabel *parkLbl;
+@property (weak, nonatomic) IBOutlet UILabel *pickUpLbl;//接机
+@property (weak, nonatomic) IBOutlet UILabel *fitnessLbl;//健身
+@property (weak, nonatomic) IBOutlet UILabel *freeProductLbl;//免费洗漱品
+@property (weak, nonatomic) IBOutlet UILabel *freeWifi;
+@property (weak, nonatomic) IBOutlet UILabel *wakeUpLbl;
+@property (weak, nonatomic) IBOutlet UILabel *restaurantLbl;
+@property (weak, nonatomic) IBOutlet UILabel *luggageLbl;//行李寄存
+@property (weak, nonatomic) IBOutlet UILabel *checkInTimeLbl;
+@property (weak, nonatomic) IBOutlet UILabel *leaveTimeLbl;
+@property (weak, nonatomic) IBOutlet UILabel *isCarrayPetLbl;
+- (IBAction)roomTypeAction:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)ChatAction:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)purchaseAction:(UIButton *)sender forEvent:(UIEvent *)event;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
+@property (weak, nonatomic) IBOutlet UIDatePicker *dataPicker;
 
 @end
 
@@ -34,4 +63,12 @@
 }
 */
 
+- (IBAction)roomTypeAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
+
+- (IBAction)ChatAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
+
+- (IBAction)purchaseAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
 @end
